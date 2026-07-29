05ae36d7b1ab904d4195e73c9d103ca0b4c936b0
João Vitor Marques <jvsilvam@outlook.com>
2026-07-29 11:53:53 -0300

    feat(user): add email uniqueness validation to create user use case

---
+++ src/main/java/com/joaovtmarques/auth/domain/user/exception/UserAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.joaovtmarques.auth.domain.user.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+  public UserAlreadyExistsException(String message) {
+    super(message);
+  }
+
+}
--- src/main/java/com/joaovtmarques/auth/domain/user/usecase/create/CreateUserUseCaseImpl.java
+++ src/main/java/com/joaovtmarques/auth/domain/user/usecase/create/CreateUserUseCaseImpl.java
@@ -3,6 +3,7 @@ package com.joaovtmarques.auth.domain.user.usecase.create;
 import com.joaovtmarques.auth.domain.user.model.User;
 import com.joaovtmarques.auth.domain.user.repository.UserRepository;
 import com.joaovtmarques.auth.domain.user.service.PasswordEncryptor;
+import com.joaovtmarques.auth.domain.user.exception.UserAlreadyExistsException; // Exceção de domínio
 
 public class CreateUserUseCaseImpl implements CreateUserUseCase {
 
@@ -16,9 +17,12 @@ public class CreateUserUseCaseImpl implements CreateUserUseCase {
 
   @Override
   public User execute(CreateUserCommand command) {
+    if (userRepository.findByEmail(command.email()).isPresent()) {
+      throw new UserAlreadyExistsException("Email already registered: " + command.email());
+    }
+
     String encryptedPassword = passwordEncryptor.encrypt(command.password());
     User user = command.toModel(encryptedPassword);
     return userRepository.save(user);
   }
-
 }
